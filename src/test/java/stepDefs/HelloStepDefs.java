06e90cd67b0b1b1515bd30e5865f6fbf6966416f
Dumtru Chicu <dumitru.chicu@gmail.com>
2019-03-21 10:34:23 -0700

    Make the scenario moree expressive

--- src/test/java/stepDefs/HelloStepDefs.java
+++ src/test/java/stepDefs/HelloStepDefs.java
@@ -6,18 +6,18 @@ import cucumber.api.java.en.When;
 
 public class HelloStepDefs {
 
-    @Given("John is a new community user")
-    public void i_am_a_new_community_user() {
+    @Given("{string} is a new community user")
+    public void a_new_community_user(String memberName) {
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @When("I say {string} to the community")
-    public void i_say_to_the_community(String string) {
+    @When("(he|she) says {string} to the community")
+    public void he_welcomes_the_community(String welcomeMessage) {
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @Then("community replies with greetings")
-    public void community_replies_with_greetings() {
+    @Then("community replies with {string}")
+    public void community_replies_with_greetings(String greetingMessage) {
         // Write code here that turns the phrase above into concrete actions
     }
 }
